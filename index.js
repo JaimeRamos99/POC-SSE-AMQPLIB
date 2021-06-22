138373756cf5df817e609146c3c03b7d92967461
Jaime Ramos <jrossetes@gmail.com>
2021-06-22 17:11:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const consumer = require('./consumer');
+const publisher = require('./publisher');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+consumer.start();
+publisher.start();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/status', (request, response) => response.json({clients: clients.length}));
+
+const PORT = 3000;
+
+let clients = [];
+let facts = [];
+
+app.listen(PORT, () => {
+  console.log(`Facts Events service listening at http://localhost:${PORT}`)
+})
+
+function eventsHandler(request, response, next) {
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+  
+    const data = `data: ${JSON.stringify(facts)}\n\n`;
+  
+    response.write(data);
+  
+    const clientId = Date.now();
+  
+    const newClient = {
+      id: clientId,
+      response
+    };
+  
+    clients.push(newClient);
+  
+    request.on('close', () => {
+      console.log(`${clientId} Connection closed`);
+      clients = clients.filter(client => client.id !== clientId);
+    });
+  }
+
+  async function addFact(request, respsonse, next) {
+    const newFact = request.body;
+    publisher.publish("", "jobs", new Buffer.from(newFact.info))
+    facts.push(newFact);
+    respsonse.json(newFact)
+    return sendEventsToAll(newFact);
+  }
+
+
+  //routes
+
+  app.get('/events', eventsHandler);
+  function sendEventsToAll(newFact) {
+    clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+  }
+
+  app.post('/fact', addFact);
---
+++ publisher.js
@@ -0,0 +1,93 @@
+// Access the callback-based API
+var amqp = require('amqplib/callback_api');
+var amqpConn = null;
+var CLOUDAMQP_URL = process.env.CLOUDAMQP_URL;
+var pubChannel = null;
+
+//offlinePubQueue is an internal queue for messages that could not be sent when the application was offline.
+// The application will keep an eye on this queue and try to resend any messages added to it.
+var offlinePubQueue = [];
+                                                                                                                                                               
+//stablish a connection to RabbitMQ. If the connection is closed or fails to be established, 
+//it will try to reconnect. amqpConn will hold the connection and channels will be set up in the connection.
+function start() {
+    amqp.connect(CLOUDAMQP_URL + "?heartbeat=60", function(err, conn) { 
+        if (err) { 
+            console.error("[AMQP]", err.message);
+            return setTimeout(start, 1000);
+        }
+
+        conn.on("error", function(err) {
+            if (err.message !== "Connection closing") {
+                console.error("[AMQP] conn error", err.message); 
+            }
+        });
+
+        conn.on("close", function() {
+            console.error("[AMQP] reconnecting");
+            return setTimeout(start, 1000);
+        });
+
+        console.log("[AMQP] publisher connected");
+        amqpConn = conn;
+        startPublisher();
+        console.log('publisher is started');
+    });
+}
+
+// createConfirmChannel opens a channel which uses "confirmation mode". 
+// A channel in confirmation mode requires each published message to be ‘acked’ or ‘nacked’ by the server, 
+// thereby indicating that it has been handled.
+function startPublisher() {
+    amqpConn.createConfirmChannel(function(err, ch) {
+        
+        if (closeOnErr(err)) return;
+        ch.on("error", function(err) { 
+            console.error("[AMQP] channel error", err.message); 
+        });
+        
+        ch.on("close", function() { 
+            console.log("[AMQP] channel closed"); 
+        });
+        
+        pubChannel = ch; 
+        while (true) {
+            var m = offlinePubQueue.shift();
+            if (!m) break;
+            publish(m[0], m[1], m[2]); 
+        }
+    });
+}
+
+// method to publish a message, will queue messages internally if the connection is down and resend later
+
+function publish(exchange, routingKey, content) {
+    try {
+        pubChannel.publish(exchange, routingKey, content, { persistent: true }, 
+            function(err, ok) { 
+                if (err) { 
+                    console.error("[AMQP] publish", err);
+                     offlinePubQueue.push([exchange, routingKey, content]);
+                     pubChannel.connection.close(); 
+                } 
+            });
+        
+    } catch (e) { 
+        console.error("[AMQP] publish", e.message); 
+        offlinePubQueue.push([exchange, routingKey, content]); 
+    }
+        
+}
+
+function closeOnErr(err) { 
+    if (!err) return false; 
+    console.error("[AMQP] error", err); 
+    amqpConn.close(); 
+    return true;
+}
+
+/*setInterval(function() { 
+    publish("", "jobs", new Buffer.from("work work work")); 
+}, 1000);
+*/
+module.exports = {start, publish}
